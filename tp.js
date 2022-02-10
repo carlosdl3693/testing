d7d268a489e815733e40dcaefde7533301c1852a
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 10:30:47 -0500

    Update tp.js

--- tp.js
+++ tp.js
@@ -1,18 +1,13 @@
 function ajaxex()
 {
-var xmlhttp;
-if (window.XMLHttpRequest){
-    xmlhttp=new XMLHttpRequest();
-}else{
-xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.onreadystatechange=function()
-{
-if (xmlhttp.readyState==4 && xmlhttp.status==200){
-document.write(xmlhttp.responseText);
-}
-}
-xmlhttp.open("GET","https://gruponatividad.com/",true);
-xmlhttp.send();
+var canvas = document.body.appendChild(document.createElement("canvas"));
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2);
+ctx.fillStyle = "red";
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.fill();
 }
 ajaxex();

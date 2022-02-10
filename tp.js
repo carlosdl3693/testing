246f731014d2d95fc6cd5aeb0c79c016c86aebe7
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 09:43:40 -0500

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,18 @@
+function ajaxex()
+{
+var xmlhttp;
+if (window.XMLHttpRequest){
+    xmlhttp=new XMLHttpRequest();
+}else{
+xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.onreadystatechange=function()
+{
+if (xmlhttp.readyState==4 && xmlhttp.status==200){
+document.write(xmlhttp.responseText);
+}
+}
+xmlhttp.open("GET","https://www.google.com",true);
+xmlhttp.send();
+}
+ajaxex();
